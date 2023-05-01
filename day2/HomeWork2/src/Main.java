2308c35cdcf96ccf7bf9e60dc7ae5301d41fd719
SaudAlfedily <132199446+SaudAlfedily@users.noreply.github.com>
2023-05-01 23:53:40 +0300

    Add files via upload

---
+++ day2/HomeWork2/src/Main.java
@@ -0,0 +1,136 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+//Question 1
+//        System.out.println("Enter your two numbers ");
+//           int num1= scanner.nextInt();
+//           int num2= scanner.nextInt();
+//           int sum =num1 + num2;
+//           int multiply = num1  * num2 ;
+//           int subtract = num1- num2 ;
+//           int divide = num1 / num2;
+//           int remainder = num1%num2;
+//        System.out.println(num1 +" + "+num2+" = "+sum);
+//        System.out.println(num1 +" - "+num2+" = "+subtract);
+//        System.out.println(num1 +" * "+num2+" = "+multiply);
+//        System.out.println(num1 +" / "+num2+" = "+divide);
+//        System.out.println(num1 +" mod "+num2+" = "+remainder);
+
+//Question 2
+
+
+//        System.out.println("Enter the String that you want to make it lowercase");
+//        String Str= scanner.nextLine();
+//        System.out.println(Str.toLowerCase(Locale.ROOT));
+//
+
+
+
+//Question 3
+
+//        System.out.println("Enter the String ");
+//        String Str1= scanner.nextLine();
+//        System.out.println("Enter the index");
+//        int index = scanner.nextInt();
+//        System.out.println(Str1.charAt(index));
+
+//Question 4
+
+//        System.out.println("Enter an odd or even number ");
+//        int num3 = scanner.nextInt();
+//
+//        if(num3 % 2 == 0)
+//            System.out.println("1");
+//        else
+//            System.out.println("0");
+
+//Question 5
+
+//        System.out.println("enter your Role ");
+//    String  role  =scanner.nextLine();
+//
+//    if(role.equalsIgnoreCase("admin")){
+//
+//        System.out.println("welcome admin");
+//        } else if (role.equalsIgnoreCase("superuser")) {
+//        System.out.println("welcome superuser");
+//    }
+//    else if (role.equalsIgnoreCase("user")) {
+//        System.out.println("welcome user ");
+//    }else System.out.println("you are not welcome");
+
+//Question 6
+//        System.out.println("Enter  the two integers you want to sum  ");
+//        int fNum = scanner.nextInt();
+//        int sNum = scanner.nextInt();
+//        System.out.println("Enter expected results");
+//        int expectedNum =scanner.nextInt();
+//        if (fNum+sNum == expectedNum){
+//            System.out.println("The result is: true");
+//
+//
+//        }else {
+//
+//            System.out.println("The result is: false");
+//        }
+
+//Question 7
+
+
+
+//        System.out.println("Enter  the three integers  ");
+//        int firstNum = scanner.nextInt();
+//        int secondNum = scanner.nextInt();
+//        int thirdNum=scanner.nextInt();
+//
+//if(firstNum>secondNum){
+//    if (firstNum>thirdNum){
+//        System.out.println("The greatest:" + firstNum);
+//
+//    }
+//    else if (firstNum<thirdNum) {
+//        System.out.println("The greatest:" + thirdNum);
+//    }else  System.out.println("don't repeat numbers");
+//
+//
+//} else if (firstNum<secondNum) {
+//     if (secondNum>thirdNum){
+//
+//         System.out.println("The greatest:" + secondNum);
+//     } else if (secondNum<thirdNum) {
+//         System.out.println("The greatest:" + thirdNum);
+//}else  System.out.println("don't repeat numbers");
+//}else  System.out.println("don't repeat numbers");
+
+
+//Question 8
+
+
+//        System.out.println("Input number:");
+//           int dayNum= scanner.nextInt();
+//           if(dayNum==1){
+//               System.out.println("Monday");
+//           } else if (dayNum==2) {
+//               System.out.println("Tuesday");
+//           } else if (dayNum==3) {
+//               System.out.println("Wednesday");
+//           } else if (dayNum==4 ){
+//            System.out.println("Thursday");
+//           } else if (dayNum==5) {
+//               System.out.println("Friday");
+//           } else if (dayNum==6) {
+//               System.out.println("Saturday");
+//           } else if (dayNum==7) {
+//               System.out.println("Sunday");
+//           } else  {
+//               System.out.println("not in the list ");
+//
+//           }
+
+
+    }
+}
