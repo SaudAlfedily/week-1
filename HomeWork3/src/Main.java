eec615192262493cb1198e76881650c187a3bb5d
SaudAlfedily <132199446+SaudAlfedily@users.noreply.github.com>
2023-05-02 19:51:11 +0300

    Add files via upload

---
+++ HomeWork3/src/Main.java
@@ -0,0 +1,235 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[])  //static method
+    {
+
+        Scanner scanner = new Scanner(System.in);
+//Question 1
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//
+//                System.out.println("FizzBuzz" );
+//            } else if (i % 3 == 0) {
+//
+//
+//                System.out.println("Fizz" );
+//            } else if (i % 5 == 0) {
+//
+//                System.out.println("Buzz" );
+//
+//            }else {
+//                System.out.println(i);
+//            }
+//
+//
+//
+//        }
+//Question 2
+
+//        System.out.println("enter String you want to revers");
+//        String Word=scanner.nextLine();
+//        char singel;
+//       String revers="";
+//
+//        for (int i=0; i<Word.length(); i++){
+//            singel= Word.charAt(i);
+//            revers= singel+revers;
+//
+//        }
+//        System.out.println(revers);
+
+
+
+
+//Question 3
+
+
+//        System.out.println("enter positive integer");
+//        int Pnum=scanner.nextInt();
+//if(Pnum>=0){
+//        for(int i=0; i<=10;i++){
+//            int multy = Pnum * i;
+//            System.out.println(Pnum+ " x "+i+" ="+multy);}}
+//        else{
+//    System.out.println("please enter positive");
+//}
+
+
+//Question 4
+
+//        System.out.println("enter number you want his factorial");
+//        int Fnum=scanner.nextInt();
+//        int factorial=1;
+//        for (int i=1;i<=Fnum;i++){
+//
+//            factorial= factorial*i;
+//
+//
+//        }
+//        System.out.println(factorial);
+
+
+ //Question 5
+
+
+//        System.out.println("enter the base number ");
+//        int Bnum=scanner.nextInt();
+//        System.out.println("enter the power");
+//        int Ponum=scanner.nextInt();
+//        int BPnum=1;
+//
+//       for (int i=1 ;i<=Ponum;i++){
+//
+//
+//           BPnum *=Bnum;
+//
+//
+//
+//
+//
+//       }
+//        System.out.println("the answer "+ BPnum);
+
+
+//question 6
+
+//        System.out.println("enter number you want to sum -1 to see answer");
+//        int sumOdd= 0;
+//        int sumEven= 0;
+//
+//
+//        int SSnum=scanner.nextInt();
+//        while(SSnum!=-1){
+//
+//            if (SSnum % 2 == 0)
+//                sumEven = sumEven + SSnum;
+//            else
+//                sumOdd = sumOdd + SSnum;
+//            System.out.println("enter number you want to sum -1 to see answer");
+//            SSnum=scanner.nextInt();
+//
+//        }
+//        System.out.println("Even sum: " + sumEven);
+//        System.out.println("Odd sum: " + sumOdd);
+
+// Question 7
+
+//        System.out.println("enter positive number");
+//        int Prime= scanner.nextInt();
+//        boolean isPrime =true;
+//
+//       for(int counter =2;counter<Prime;counter++){
+//
+//           if (Prime % counter == 0) {
+//               isPrime = false;
+//               break;
+//           }
+//       }
+//       if (isPrime)
+//        System.out.println(Prime + " is a prime number.");
+//    else
+//        System.out.println(Prime + " is not a prime number.");
+
+//Question 8
+
+//        System.out.println("enter numbers  -1 to exit ");
+//        int Positve = 0;
+//        int Negative= 0;
+//        int zeros=0;
+//        int Allnum=scanner.nextInt();
+//        while(Allnum!=-1){
+//
+//            if(Allnum==0){
+//                zeros++;
+//            } else if (Allnum>0) {
+//                Positve++;
+//
+//            }else{
+//
+//                Negative++;
+//            }
+//            System.out.println("enter numbers  -1 to exit ");
+//            Allnum=scanner.nextInt();
+//
+//
+//        } System.out.println(" Positve = "+Positve +" Negative = "+ Negative + " zeros ="+ zeros);
+
+
+//Question 9
+
+//        int  week=4;
+//       int days=7;
+//
+//        for(int i= 1;i<=week;i++){
+//            System.out.println("week"+i);
+//            for (int j=1; j<=days;j++){
+//
+//                System.out.println("Day"+j);
+//            }
+//
+//
+//        }
+
+//question 10
+
+
+//                System.out.println("enter String you want to revers");
+//        String Word=scanner.nextLine();
+//        char singel;
+//       String revers="";
+//
+//        for (int i=0; i<Word.length(); i++){
+//            singel= Word.charAt(i);
+//            revers= singel+revers;
+//
+//        }
+//        if (revers.equalsIgnoreCase(Word)){
+//
+//            System.out.println("palindrome");
+//        }else {
+//
+//            System.out.println("not palindrome");
+//        }
+
+
+        }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
